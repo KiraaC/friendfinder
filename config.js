8768d5e8257c104ccbc03e9b57cd5bab81361377
Kira Corbin <madkatkeys@gmail.com>
2019-07-06 19:17:52 -0700

    cleaned up format on config.js

--- config.js
+++ config.js
@@ -1,27 +1,14 @@
-const express = require('express');
-const router = express.Router();
-
-const apiRoutes = require('./main');
-
-router.use('/main', apiRoutes);
-router.get('/', (req, res) => res.send("link to main.js"));
-
-module.exports = router;
-
-
-// router
-router.get('/', (req, res) => {
-        connection.query('SELECT * from todos', (err, todos) => {
-            res.json(todos);
-        });
-    });
-
-// server
 const express = require("express");
-const PORT = process.env.PORT || 3306;
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT,() => console.log("server started on port $(PORT)"));
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
